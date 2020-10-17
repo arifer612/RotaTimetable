0c3043c529ecd37f9449bce0f28ad45c408914ee
arifer@linux <arifer1995@gmail.com>
2020-10-17 20:37:55 +0800

    Cleared up Rota object (TODO)

--- RotaTimeTable.py
+++ RotaTimeTable.py
@@ -13,96 +13,160 @@ import sys
 
 
 class Rota:
-    def __init__(self, rota=None, fileName=None, rootDir=None):
+    def __init__(self, rota, fileName=None, rootDir="."):
         if not any([rota, fileName]):
-            sys.exit('Provide rota to create a new rota, or log file to load data')
+            sys.exit('Provide rota number to create a new rota or log file directory to load data')
         else:
-            self.rota, self.fileName, self.rootDir = rota, fileName, rootDir
-            self.personnel, self.appliances = [], []
-            if self.fileName:
-                self.load()
-        
+            self.rota, self._fileName, self._rootDir = rota, fileName, rootDir
+            self._personnel, self._appliances = [], []
+            if self._fileName:
+                self._load()
+            else:
+                self._fileName = f"Rota {self.rota}.data"
+                self._rootDir = rootDir
+
+    @property
+    def personnel(self):
+        return self._personnel
+
+    @property
+    def _personnelName(self):
+        return [i.name for i in self._personnel]
+
+    @property
+    def appliances(self):
+        return self._appliances
+
+    @property
+    def _applianceName(self):
+        return [i.name for i in self._appliances]
+
     def __add__(self, other):
         if self.rota != other.rota:
-            raise TypeError
+            raise TypeError("Only same rotas can be joined together")
         else:
-            self.personnel += other.personnel
-            self.appliances += other.appliances
-        
+            self.personnel += [i for i in other.personnel if i.name not in self.personnel]
+            self.appliances = self.appliances | other.appliances
+
     def __sub__(self, other):
         if self.rota != other.rota:
             print("ERROR: You can only add the same rota together")
         else:
-            self.personnel = [i for i in self.personnel 
+            self.personnel = [i for i in self.personnel
                               if i not in other.personnel]
             self.appliances = [i for i in self.appliances
                                if i not in other.appliances]
-        
+
+    def __len__(self):
+        return len(self.personnel)
+
+    def __repr__(self):
+        return f"Rota {self.rota}"
+
+    def __str__(self):
+        return f"ROTA {self.rota}\n" \
+               f"PERSONNEL: {len(self)}\n" \
+               f"APPLIANCES: {self._checkActive()[0]} on-run; {self._checkActive()[1]} off-run"
+
     def addPersonnel(self, person):
-        if type(person) != Personnel:
-            print(f"ERROR: {person} does not exist. Create an entry for {person} with Personnel()")
+        if type(person) == list:
+            if not all([type(i) == Personnel for i in person]):
+                print("ERROR: Some personnel in list do not exist. Create entry using Personnel()")
+            else:
+                [self.personnel.append(i) for i in person if i not in self.personnel]
+        elif type(person) != Personnel:
+            print(f"ERROR: {person} does not exist. Create an entry for {person} using Personnel()")
         elif person not in self.personnel:
             self.personnel.append(person)
         else:
             pass
-            
+
+    def removePersonnel(self, person):
+        if type(person) == list:
+            if not all([type(i) == Personnel for i in person]):
+                print("ERROR: Some personnel in list do no exist. Remove them from list to continue?\n")
+                print(f"REMOVE -> {', '.join([i for i in person if type(i) != Personnel])}")
+            else:
+                [self.personnel.remove(i) for i in person if i in person]
+        elif type(person) != Personnel:
+            print(f"ERROR: {person} does not exist.")
+        elif person in self.personnel:
+            self.personnel.remove(person)
+        else:
+            pass
+
     def addRole(self, person, role):
         if person not in self.personnel:
             print(f"ERROR: {person} does not exist. Create an entry for {person} with Personnel()")
         else:
             person.addRole(role)
-            
+
     def addAppliance(self, appliance):
-        if type(appliance) != Appliance:
+        if type(appliance) == list:
+            if not all([type(i) == Appliance for i in appliance]):
+                print(f"ERROR: Some appliance(s) in list do not exist. Create using Appliance()")
+            else:
+                [self.appliances.append(i) for i in appliance if i not in self.appliances]
+        elif type(appliance) != Appliance:
             print(f"ERROR: {appliance} does not exist. Create {appliance} using Appliance()")
         elif appliance not in self.appliances:
             self.appliances.append(appliance)
         else:
             pass
-            
-    def setRule(self, appliance, rule):
-        if appliance not in self.appliances:
-            print(f"ERROR: {appliance} does not exist. Create {appliance} using Appliance()")
+
+    def removeAppliance(self, appliance):
+        if type(appliance) == list:
+            if not all([type(i) == Appliance for i in appliance]):
+                print("ERROR: Some appliance(s) in list do not exist. Remove them to continue?\n")
+                print(f"REMOVE -> {', '.join([i for i in appliance if type(i) != Appliance])}")
+            else:
+                [self.personnel.remove(i) for i in appliance if i in self.personnel]
+        elif type(appliance) != Personnel:
+            print(f"ERROR: {appliance} does not exist.")
+        elif appliance in self.appliances:
+            self.appliances.remove(appliance)
         else:
-            appliance.setRule(rule)
-            
+            pass
+
     def setPriority(self, appliance, priority):
         if appliance not in self.appliances:
             print(f"ERROR: {appliance} does not exist. Create {appliance} using Appliance()")
         else:
             appliance.setPriority(priority)
-            
-    def save(self):
-        if not self.fileName:
-            self.fileName = f"Rota {self.rota}"
-        if not self.rootDir:
-            self.rootDir = os.path.abspath(".")
-        data = {self.rota: self.personnel}
+
+    def setRule(self, target, rule):
+        if target not in (self.appliances, self.personnel):
+            print(f"ERROR: {target} does not exist.")
+        else:
+            target.setRule(rule)
+
+    def _save(self, fileName=None, rootDir=None):
+        if fileName:
+            self.fileName = os.path.splitext(fileName)[0] + ".data"
+        if rootDir:
+            self.rootDir = rootDir
+        data = {'rota': self.rota, 'personnel': self.personnel, 'appliances': self.appliances}
         with open(os.path.abspath(os.path.expanduser(os.path.join(self.rootDir, self.fileName))), 'wb') as f:
             pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
-            
-    def load(self):
-        if not os.path.abspath(os.path.expanduser(os.path.join(self.rootDir, self.fileName))):
+
+    def _load(self):
+        file = os.path.splitext(os.path.join(self.rootDir, self.fileName))[0] + ".data"
+        if not os.path.exists(os.path.abspath(os.path.expanduser(file))):
             print("File does not exist. Creating new rota.")
-            self.personnel = []
         else:
-            with open(os.path.abspath(os.path.expanduser(os.path.join(self.rootDir, self.fileName))), 'rb') as f:
+            with open(os.path.abspath(os.path.expanduser(file)), 'rb') as f:
                 data = pickle.load(f)
-            self.rota = list(data)[0]
-            self.personnel = list(data.values())[0]
-        
-        
-class Personnel:
-    def __init__(self, name, role=None):
-        self.ID = 0
-        self.__name__, self._role = name, role
-        self.role = None if not self._role else self._role.role
-        
-    def addRole(self, role):
-        if type(role) != Role:
-            print(f"ERROR: {role} does not exist. Create {role} with Role()")
-class Role:
-    def __init__(self, name: str, constraint: Union[dict, list, str, None]=None, rule: bool=False, inherit: [list, Role, None]=None):
+            self.rota = data['rota']
+            self.personnel = data['personnel']
+            self.appliances = data['appliances']
+
+    def print(self):
+        return print(self)
+
+    def _checkActive(self):
+        return len([i for i in self.appliances if i.active]), len([i for i in self.appliances if not i.active])
+
+
 class Role(object):
     def __init__(self, name, constraint=None, rule=False, inherit=None):
         self.role = name
