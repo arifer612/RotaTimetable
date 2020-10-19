f76153e5487333dafd1952e1a251d96d296d6c8c
arifer@linux <arifer1995@gmail.com>
2020-10-19 23:36:35 +0800

    Added typing hints and docstrings to Role, Personnel, and Appliance

--- RotaTimeTable.py
+++ RotaTimeTable.py
@@ -11,13 +11,39 @@ import pickle
 import os
 import sys
 import hashlib
-from typing import Union
+from typing import Union, Dict, Tuple, NewType
 
 
 class Role(object):
+    """
+    Creates an object that defines the Role of a Personnel.
+    ...
+        Attributes:
+            role (str): Name of the Role.
+            constraints (dict): Dictionary of Vehicles and rules. Shows which vehicle the
+                                current Role has access to. The constraints are retrieved
+                                from 2 sources -- itself and its inherited role. The
+                                inherited constraints do not take precedence over its
+                                declared constraints.
+    ...
+    """
     def __init__(self, name,
-                 constraint: Union[dict, list, str] = None, rule: bool = False,
+                 constraint: Union["Vehicle", dict, list] = None, rule: bool = False,
                  inherit: "Role" = None):
+
+        """
+        Arguments:
+            name (str): Name of the Role
+            constraint (dict, list, Vehicle): Constraints of this Role.
+                (dict): Adds the whole dictionary of constraints as this Role's constraints.
+                (list): Adds the list of Vehicles to this Role's constraints.
+                (Vehicle): Adds the Vehicle to this Role's constraints.
+            rule (bool): Rule for the constraint(s) if constraint is a list or Vehicle.
+            inherit (Role): Inherits the constraints of this Role.
+
+        Raise:
+            TypeError: If arguments are of the incorrect type.
+        """
         self.role, self._constraints, self._inheritance = name, {}, inherit
 
         if not isinstance(rule, bool):
@@ -28,30 +54,67 @@ class Role(object):
                 raise TypeError("Only <Role> can be inherited")
 
         if constraint:
-            if isinstance(constraint, dict):
-                for i, j in constraint:
-                    if not isinstance(i, Vehicle):
-                        raise TypeError("Only <Vehicle> can be added as constraints")
-                    if not isinstance(j, bool):
-                        raise TypeError("Only <boolean> rules are allowed")
-                    self._constraints.update(constraint)
-            elif isinstance(constraint, list):
-                self._constraints.update({i: rule for i in constraint})
-            else:
-                self._constraints.update({constraint: rule})
+            self.constraint(constraint)
 
     @property
-    def constraints(self) -> dict:
+    def constraints(self) -> Dict["Vehicle", bool]:
+        """
+        Updates the Role's constraints if the inherited Role has a new constraint unless
+        declared beforehand. The declaration is usually made when creating the instance
+        and may be done post-creation through the constraint() method.
+        """
         if self._inheritance:
             return {**self._inheritance.constraints, **self._constraints}
         else:
             return self._constraints
 
-    def constraint(self, constraint=None, rule=False):
-        if constraint:
-            self._constraints.update({constraint: rule})
+    def constraint(self, constraint: Union["Vehicle", dict, list], rule: bool = False):
+        """
+        Declares a new constraint for this Role.
+        ...
+            Arguments:
+               constraint (dict, list, Vehicle): Constraints of this Role.
+                   (dict): Adds the whole dictionary of constraints as this Role's constraints.
+                   (list): Adds the list of Vehicles to this Role's constraints.
+                   (Vehicle): Adds the Vehicle to this Role's constraints.
+               rule (bool): Rule for the constraint(s) if constraint is a list or Vehicle.
+        ...
+        """
+        if isinstance(constraint, dict):
+            err = erri = errj = []
+            for i, j in constraint.items():
+                if not isinstance(i, Vehicle):
+                    erri.append(i)
+                if not isinstance(j, bool):
+                    errj.append(j)
+                if erri:
+                    err.append(f"{', '.join(erri)}: Only <Vehicle> can be added as constraints")
+                if errj:
+                    err.append(f"{', '.join(errj)}: Only <boolean> rules are allowed")
+                if err:
+                    raise TypeError(f"{'& '.join(err)}")
 
-    def __add__(self, other):
+            self._constraints.update(constraint)
+        elif isinstance(constraint, "Vehicle"):
+            if not isinstance(rule, bool):
+                raise TypeError("Only <boolean> rules are allowed")
+            self._constraints.update({constraint: rule})
+        elif isinstance(constraint, list):
+            errs = []
+            for i in constraint:
+                if not isinstance(i, Vehicle):
+                    errs.append(i)
+            if errs:
+                raise TypeError(f"{', '.join(errs)}: Only <Vehicle> can be added as constraints")
+
+            [self._constraints.update({i: rule}) for i in constraint]
+        else:
+            raise TypeError("Only <Vehicle> can be added as constraints")
+
+    def __add__(self, other: "Role"):
+        """
+        Defines a one-way operation to add the constraint of an existing Role to the current Role.
+        """
         if not isinstance(other, Role):
             raise TypeError("Only <Role> can be added to <Role>")
         else:
@@ -68,16 +131,38 @@ class Role(object):
     def __str__(self):
         return f"{self.constraints if self.constraints else 'NONE'}"
 
-    def __call__(self):
+    def __call__(self) -> Tuple[str, Dict["Vehicle", bool], str]:
+        """
+        Returns human-readable data.
+        """
         return self.role, self.constraints, self._inheritance.role
 
 
 class Personnel(Role):
+    """
+    Creates a Personnel using an existing Role
+    ...
+        Attributes:
+            name (str): Name of the Personnel.
+            id (str): 8-digits unique hash of the Personnel.
+            role (str): Name of the Personnel's Role.
+            constraints (dict): Dictionary of Vehicles and rules. Shows which vehicle the
+                                current Role has access to. The constraints are retrieved
+                                from 2 sources -- itself and its inherited role. The
+                                inherited constraints do not take preceedence over its
+                                declared constraints.
+    ...
+    """
     def __init__(self, name, role: Role):
+        """
+        Arguments:
+            name (str): Name of the Personnel.
+            role (Role): Inherits the properties and attributes of the Role.
+        """
         self._name = name
         if not isinstance(role, Role):
             raise TypeError("Only <Role> can be used to create personnel")
-        super().__init__(role)
+        super().__init__(role.role, inherit=role)
 
     @property
     def name(self):
@@ -85,19 +170,21 @@ class Personnel(Role):
 
     @name.setter
     def name(self, newName):
+        """
+        Changes the name of the Personnel.
+        """
         self._name = newName
 
     @property
-    def __id(self):
+    def __id(self) -> str:
         return hashlib.sha1(f"{self.name} @ {self.role}".encode()).hexdigest()
 
     @property
-    def id(self):
+    def id(self) -> str:
+        """
+        8 digits slice of the unique hash of the Personnel.
+        """
         return self.__id[10:17]
-
-    @property
-    def constraints(self):
-        return self.role.constraints
 
     def __repr__(self):
         return self.id
@@ -105,7 +192,10 @@ class Personnel(Role):
     def __str__(self):
         return self.name
 
-    def __call__(self):
+    def __call__(self) -> Dict[str, Union[str, Dict["Vehicle", bool]]]:
+        """
+        Returns human-readable data.
+        """
         return {
             'name': self.name,
             'role': self.role,
@@ -114,15 +204,52 @@ class Personnel(Role):
 
 
 class Appliance(object):
+    """
+    Creates an object that defines the Appliance of a Vehicle.
+    ...
+        Attributes:
+            appliance (str): Name of the Appliance.
+            crew (dict): Dictionary of Roles and their maximum number in the Appliance.
+            limits (list): Minimum and maximum number of Personnel in the Appliance.
+    ...
+    """
     def __init__(self, name, crew: dict, minimum: int = 1, maximum: int = 1):
+        """
+        Arguments:
+            name (str): Name of the Appliance.
+            crew (dict): Crew allowed on the Appliance.
+            minimum (int): Minimum number of Personnel required on the Appliance.
+            maximum (int): Maximum number of Personnel allowed on the Appliance.
+
+        Raise:
+            TypeError: Arguments are of the incorrect types.
+            ValueError: Crew not added or maximum is more than minimum.
+        """
         if not crew:
             raise ValueError(f"Assign minimum crew of {name}")
         elif not isinstance(crew, dict):
             raise TypeError("Crew has to be represented as a <dict>")
+        if not (isinstance(minimum, int) and isinstance(maximum, int)):
+            raise TypeError("Minimum and Maximum have to be <integer>")
         if maximum < minimum:
             raise ValueError(f"Maximum cannot be less than the minimum")
         maximum = maximum if maximum >= sum(crew.values()) else sum(crew.values())
-        self._appliance, self._crew, self.limits = name, crew, [minimum, maximum]
+        self._appliance, self._crew, self._limits = name, crew, [minimum, maximum]
+
+    @property
+    def limits(self):
+        return self._limits
+
+    @limits.setter
+    def limits(self, newLimits):
+        """
+        Changes the crew limits on the Appliance.
+        """
+        if not isinstance(newLimits, (list, tuple)):
+            raise TypeError(f"Limits have to be have to be represented as a list or tuple")
+        if len(newLimits) != 2:
+            raise ValueError("Incorrect number of limits")
+        self._limits = list(newLimits)
 
     @property
     def appliance(self):
@@ -130,43 +257,77 @@ class Appliance(object):
 
     @appliance.setter
     def appliance(self, newName):
+        """
+        Changes the name of the Appliance.
+        """
         self._appliance = newName
 
     @property
-    def crew(self):
+    def crew(self) -> Dict[Role, int]:
         self._crew = {i: v for (i, v) in self._crew.items() if v}
         return self._crew
 
     def __repr__(self):
         return f"{self.crew}"
 
-    def __call__(self):
+    def __call__(self) -> Tuple[str, Dict[Role, int], int]:
+        """
+        Returns human-readable data.
+        """
         return (self.appliance, self.crew, *self.limits)
 
-    def change(self, role: Union[Role, dict], value: int = None, maximum: int = None):
-        self.limits[1] = maximum
-        crew = self.crew
+    def change(self, role: Union[Role, dict], value: int = None, minimum: int = None, maximum: int = None):
+        """
+        Changes the crew information of the Appliance.
+        ...
+            Arguments:
+                role (Role, dict): Roles to change
+                    (Role): Changes the number of 'Role' in the Appliance's crew to 'value'.
+                    (dict): Updates the crew with the items of the dictionary.
+                value (int): Updates 'role' in crew to 'value'
+                minimum (int): Updates minimum number of crew required on the Appliance.
+                maximum (int): Updates maximum number of crew allowed on the Appliance.
+
+            Raise:
+                TypeError: Arguments are of the incorrect types.
+                Value Error: 
+        ...
+        """
+        if minimum:
+            if not isinstance(minimum, int):
+                raise TypeError("Minimum has to be an <integer>")
+            self.limits[0] = minimum
+        if maximum:
+            if not isinstance(maximum, int):
+                raise TypeError("Maximum has to be an <integer>")
+            self.limits[1] = maximum
+
         if isinstance(role, dict):
-            for i, j in role.items():  # Check for type errors before proceeding
+            err = erri = errj = []
+            for i, j in role.items():
                 if not isinstance(i, Role):
-                    raise TypeError(f"{{{i}: {j}}}: Only <Role> can be added to crew")
+                    erri.append(i)
                 if not isinstance(j, int):
-                    raise TypeError(f"{{{i}: {j}}}: Only <integer> values are allowed")
-            crew.update(role)
-            if sum(crew.values()) > self.limits[1]:
-                raise ValueError("Total crew exceeds maximum of the appliance")
-            else:
-                self._crew.update(role)
-        elif not value:
-            raise AttributeError(f"Number of {role} has to be declared")
-        elif not isinstance(value, int):
-            raise TypeError("Only <integer> values are allowed")
+                    errj.append(j)
+            if erri:
+                err.append(f"{', '.join(erri): Only <Role> can be added to crew}")
+            if errj:
+                err.append(f"{', '.join(errj): Only <integer> values are allowed}")
+            if err:
+                raise TypeError(f"{'& '.join(err)}")
+        elif isinstance(role, Role):
+            if not value:
+                raise AttributeError(f"Number of {role} has to be declared")
+            elif not isinstance(value, int):
+                raise TypeError("Only <integer> values are allowed")
+            role = {role: value}
         else:
-            crew.update({role: value})
-            if sum(crew.values()) > self.limits[1]:
-                raise ValueError("Total crew exceeds maximum of the appliance")
-            else:
-                self._crew.update({role: value})
+            raise TypeError("Only <Role> can be added to crew.")
+
+        crew = {**self.crew, **role}
+        if sum(crew.values()) > self.limits[1]:
+            raise ValueError("Total crew exceeds maximum of the appliance")
+        self._crew.update(role)
 
 
 class Vehicle(Appliance):
